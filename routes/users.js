fd522038c986760f36b6afae2bea964037a74be0
Anna Haratova <black-isida@yandex.ru>
2023-05-19 18:42:40 +0300

    ПР14

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,7 @@ const { celebrate, Joi } = require('celebrate');
 const { urlRegex }  = require('../utils/constants')
 
 router.get('/', getUsers);
-router.get('/me', celebrate({
-  params: Joi.object().keys({
-    _id: Joi.string().length(24).hex(),
-  }),
-}), getUser);
+router.get('/me', getUser);
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
